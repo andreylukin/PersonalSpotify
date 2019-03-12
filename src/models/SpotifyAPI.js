3237d212ff9b6a70f8988075e5bbc71855159568
Andrey Lukin <luk.andrey@gmail.com>
2019-03-12 13:48:06 -0400

    Got cron job working

--- src/models/SpotifyAPI.js
+++ src/models/SpotifyAPI.js
@@ -1,22 +1,4 @@
 const SpotifyWebApi = require('spotify-web-api-node');
-const config = require("../config");
-var wikiParser = require('wiki-infobox-parser');
-
-
-async function getMyDeviceId() {
-    const response = await this.spotifyApi.getMyDevices();
-    // const device = response.body.devices.filter( device => device.name === "PHILCO")[0];// use "PHILCO" usually
-    // return device === undefined ? "Philoco aint here" : device.id;
-    if(response.body != null) {
-        return response.body.devices[0];
-    } 
-    return null;
-}
-
-async function isShuffle() {
-    const response = await this.spotifyApi.getMyCurrentPlaybackState();
-    return response.body.shuffle_state;
-}
 
 class SpotifyApi {
     constructor() {
@@ -32,86 +14,7 @@ class SpotifyApi {
         this.spotifyApi.setAccessToken(response.body['access_token']);
     }
 
-    async startMusic(context_uri) {
-        const deviceIds = await getMyDeviceId.bind(this)();
-        console.log(deviceIds);
-        await this.spotifyApi.transferMyPlayback({deviceIds: [deviceIds] })
-        await this.spotifyApi.play({device_id: deviceIds, context_uri: context_uri || undefined})
-    }
-
-    async stopMusic() {
-        const device = await getMyDeviceId.bind(this)();
-        // console.log(deviceId);
-        await this.spotifyApi.pause({device_id: device.id})
-    }
-
-    async startPlaylist(number) {
-        number = number || 0;
-        const playlists = await this.spotifyApi.getUserPlaylists();
-        const playlistURIs = playlists.body.items;
-        this.startMusic(playlistURIs[number].uri);
-    }
-
-    async getNumberofPlaylist() {
-        const playlists = await this.spotifyApi.getUserPlaylists();
-        const playlistURIs = playlists.body.items.map(item => item.uri);
-        return playlistURIs.length;
-    }
-
-    async next() {
-        await this.spotifyApi.skipToNext();
-    }
-    async previous() {
-        await this.spotifyApi.skipToPrevious();
-    }
-
-    async shuffle() {
-        const isShuffling = await isShuffle.bind(this)();
-        await this.spotifyApi.setShuffle({state: (!isShuffling).toString()});
-        console.log(`Now shuffling is ${!isShuffling}`);
-    }
 
 }
 
-
-module.exports = SpotifyApi;
-
-const spotifyApi = new SpotifyApi();
-try {
-    (async () => {
-        // await spotifyApi.authenticate();
-        // const response = await spotifyApi.spotifyApi.getMyRecentlyPlayedTracks();
-        // // console.log(response.body.items[0]);
-        // for(let i = 0; i < 1; i+=1) { // response.body.items.length
-        //     console.log(response.body.items[i]);
-        //     let name = response.body.items[i].track.name
-        //     let albumInfo = await spotifyApi.spotifyApi.getAlbum(response.body.items[i].track.album.id);
-        //     let artistInfo = await spotifyApi.spotifyApi.getArtist(response.body.items[i].track.artists[0].id);
-        //     console.log({name, albumInfo : albumInfo.body.genres.length, artistInfo: artistInfo.body.genres.length});
-        // }
-        // const trackAFInfo = await spotifyApi.spotifyApi.getAudioFeaturesForTrack(response.body.items[0].track.uri.split(":")[2]);
-        // const trackAAInfo = await spotifyApi.spotifyApi.getAudioAnalysisForTrack(response.body.items[0].track.uri.split(":")[2]);
-
-        // var page = 'Cosmo Sheldrake';
-        // var language = 'en';
-        
-        // infobox(page, language, function(err, data){
-        // if (err) {
-        //     // Oh no! Something goes wrong!
-        //     return;
-        // }
-        
-        // console.log(data);
-    // });
-    wikiParser('Cosmo Sheldrake', function(err, result) {
-        if (err) {
-              console.error(err.message);
-          } else {
-              console.log(JSON.parse(result).genre);
-          }
-      });
-    })().then(console.log)
-        .catch(console.log);
-  } catch (e) {
-    console.log(e.toString());
-}
+module.exports.SpotifyApi = SpotifyApi;
---
+++ src/models/cronCheck.js
@@ -0,0 +1,94 @@
+const config = require("../config");
+var wikiParser = require('wiki-infobox-parser');
+const fs = require('fs');
+const SpotifyApi  = require("./SpotifyApi");
+
+// async function getMyDeviceId() {
+//     const response = await this.spotifyApi.getMyDevices();
+//     // const device = response.body.devices.filter( device => device.name === "PHILCO")[0];// use "PHILCO" usually
+//     // return device === undefined ? "Philoco aint here" : device.id;
+//     if(response.body != null) {
+//         return response.body.devices[0];
+//     } 
+//     return null;
+// }
+
+
+// async function getWikipediaGenres(artist) {
+//     let response = wikiParser(artist, function(err, result) {
+//         if (err) {
+//             console.error(err.message);
+//         } else {
+//             return result;
+//         }
+//     });
+//     console.log(response);
+//     return response;
+// }
+
+
+
+function getTimestamp() {
+    let rawdata = JSON.parse(fs.readFileSync('/Users/andrey/Desktop/Projects/PersonalSpotify/src/recent-songs.json'));  
+    return new Date(rawdata.timestamp);
+}
+
+function updateTimestamp(timestamp) {
+    output = {timestamp: timestamp};
+    fs.writeFileSync('/Users/andrey/Desktop/Projects/PersonalSpotify/src/recent-songs.json', JSON.stringify(output));
+}
+
+
+try {
+    (async () => {
+        const spotifyApi = new SpotifyApi();
+        await spotifyApi.authenticate();
+        const response = await spotifyApi.spotifyApi.getMyRecentlyPlayedTracks();
+        let tracks = response.body.items;
+        let oldestRecorded = getTimestamp();
+        let currentNewest = 0;
+        for(let i = 0; i < tracks.length; i+=1) {
+            let currentTrack = tracks[i];
+
+            if(oldestRecorded < new Date(currentTrack.played_at) && new Date(currentNewest) < new Date(currentTrack.played_at)) {
+                updateTimestamp(currentTrack.played_at);
+                currentNewest = currentTrack.played_at;
+            }
+
+            if(oldestRecorded < new Date(currentTrack.played_at)) {
+                console.log(currentTrack.track.name);
+            }
+        //     // console.log(response.body.items[i]);
+        //     let name = response.body.items[i].track.name
+        //     let artist= response.body.items[i].track.artists[0].name;
+        //     // console.log(name + ": " + artist);
+        //     console.log(response.body.items[i]);
+        //     // let artistInfo = await spotifyApi.spotifyApi.getArtist(response.body.items[i].track.artists[0].id);
+        //     // console.log({name, artistInfo: artistInfo.body.genres});
+        //     // console.log(response.body.items[0].track.album.images);
+        //     // return;
+        // }
+        // const trackAFInfo = await spotifyApi.spotifyApi.getAudioFeaturesForTrack(response.body.items[0].track.uri.split(":")[2]);
+        // const trackAAInfo = await spotifyApi.spotifyApi.getAudioAnalysisForTrack(response.body.items[0].track.uri.split(":")[2]);
+
+        // var page = 'Cosmo Sheldrake';
+        // var language = 'en';
+        
+        // infobox(page, language, function(err, data){
+        // if (err) {
+        //     // Oh no! Something goes wrong!
+        //     return;
+        // }
+        
+        // console.log(data);
+    // });
+        }
+
+        // let l = getWikipediaGenres("Cosmo Sheldrake");
+
+        // console.log(response.body.items[response.body.items.length - 1]);
+    })().then(console.log)
+        .catch(console.log);
+  } catch (e) {
+    console.log(e.toString());
+}
