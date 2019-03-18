2c2f4e04bc0c239f3885e82966ed68742d81372d
Andrey Lukin <luk.andrey@gmail.com>
2019-03-17 22:41:22 -0400

    need it inside if

--- src/models/cronCheck.js
+++ src/models/cronCheck.js
@@ -67,46 +67,47 @@ try {
             if(oldestRecorded < new Date(currentTrack.played_at) && new Date(currentNewest) < new Date(currentTrack.played_at)) {
                 updateTimestamp(currentTrack.played_at, currentTrack.track.name);
                 currentNewest = currentTrack.played_at;
+                recordObj.trackInfo = {};
+                recordObj.trackInfo.name = currentTrack.track.name;
+                recordObj.trackInfo.popularity = currentTrack.track.popularity;
+                recordObj.trackInfo.uri = currentTrack.track.uri;
+
+
+                recordObj.albumInfo = {};
+                recordObj.albumInfo.name = currentTrack.track.album.name;
+                recordObj.albumInfo.album_type = currentTrack.track.album.album_type;
+                recordObj.albumInfo.release_date = currentTrack.track.album.release_date;
+                recordObj.albumInfo.total_tracks = currentTrack.track.album.total_tracks;
+
+
+                const artistInfo = await spotifyApi.spotifyApi.getArtist(currentTrack.track.artists[0].id);
+                recordObj.artistInfo = {};
+                recordObj.artistInfo.name = artistInfo.body.name;
+                recordObj.artistInfo.popularity = artistInfo.body.popularity;
+                recordObj.artistInfo.followers = artistInfo.body.followers.total;
+                recordObj.artistInfo.genres = artistInfo.body.genres;
+
+
+
+                const trackAFInfo = await spotifyApi.spotifyApi.getAudioFeaturesForTrack(currentTrack.track.uri.split(":")[2]);
+                recordObj.trackAnalysisInfo = {};
+                recordObj.trackAnalysisInfo.danceability = trackAFInfo.body.danceability;
+                recordObj.trackAnalysisInfo.energy = trackAFInfo.body.energy;
+                recordObj.trackAnalysisInfo.key = trackAFInfo.body.key;
+                recordObj.trackAnalysisInfo.mode = trackAFInfo.body.mode;
+                recordObj.trackAnalysisInfo.speechiness = trackAFInfo.body.speechiness;
+                recordObj.trackAnalysisInfo.instrumentalness = trackAFInfo.body.instrumentalness;
+                recordObj.trackAnalysisInfo.liveness = trackAFInfo.body.liveness;
+                recordObj.trackAnalysisInfo.valence = trackAFInfo.body.valence;
+                recordObj.trackAnalysisInfo.tempo = trackAFInfo.body.tempo;
+
+                allSongData.push(recordObj);
             }
 
             if(oldestRecorded < new Date(currentTrack.played_at)) {
                 console.log(currentTrack.track.name);
             }
-            recordObj.trackInfo = {};
-            recordObj.trackInfo.name = currentTrack.track.name;
-            recordObj.trackInfo.popularity = currentTrack.track.popularity;
-            recordObj.trackInfo.uri = currentTrack.track.uri;
-
-
-            recordObj.albumInfo = {};
-            recordObj.albumInfo.name = currentTrack.track.album.name;
-            recordObj.albumInfo.album_type = currentTrack.track.album.album_type;
-            recordObj.albumInfo.release_date = currentTrack.track.album.release_date;
-            recordObj.albumInfo.total_tracks = currentTrack.track.album.total_tracks;
-
-
-            const artistInfo = await spotifyApi.spotifyApi.getArtist(currentTrack.track.artists[0].id);
-            recordObj.artistInfo = {};
-            recordObj.artistInfo.name = artistInfo.body.name;
-            recordObj.artistInfo.popularity = artistInfo.body.popularity;
-            recordObj.artistInfo.followers = artistInfo.body.followers.total;
-            recordObj.artistInfo.genres = artistInfo.body.genres;
-
-
-
-            const trackAFInfo = await spotifyApi.spotifyApi.getAudioFeaturesForTrack(currentTrack.track.uri.split(":")[2]);
-            recordObj.trackAnalysisInfo = {};
-            recordObj.trackAnalysisInfo.danceability = trackAFInfo.body.danceability;
-            recordObj.trackAnalysisInfo.energy = trackAFInfo.body.energy;
-            recordObj.trackAnalysisInfo.key = trackAFInfo.body.key;
-            recordObj.trackAnalysisInfo.mode = trackAFInfo.body.mode;
-            recordObj.trackAnalysisInfo.speechiness = trackAFInfo.body.speechiness;
-            recordObj.trackAnalysisInfo.instrumentalness = trackAFInfo.body.instrumentalness;
-            recordObj.trackAnalysisInfo.liveness = trackAFInfo.body.liveness;
-            recordObj.trackAnalysisInfo.valence = trackAFInfo.body.valence;
-            recordObj.trackAnalysisInfo.tempo = trackAFInfo.body.tempo;
-
-            allSongData.push(recordObj);
+            
             
         }
         writeToRecord(allSongData);
